cc36a4d5b0a2c9f855a7c644ca750617e08af187
LuchMOl <61159984+LuchMOl@users.noreply.github.com>
2020-02-23 12:42:21 +0200

    aothorization

---
+++ autoloader.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+function myAutoloader($className){
+
+	$a = require '' . $className . '.php';
+}
+echo "$a";
+spl_autoload_register('myAutoloader');
+echo "$className";
+?>
---
+++ models/user.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form method= "post">
+		<input type="text" name="Name" placeholder="Name">
+		<input type="text" name="Email" placeholder="Email">
+		<input type="submit" value="OK">
+	</form><br>
+
+<?php
+
+		if (!empty($_POST)){
+
+				if ($_POST['Name'] == $user->UserName){
+
+					if ($_POST['Email'] == $user->UserEmail){
+					echo "Welcomе, $user->UserName!";
+					}else echo "Incorrect Email for ($_POST[Name]): $_POST[Email]";
+				}
+				else echo "Incorrect Name: $_POST[Name]";
+
+		}else echo "Enter the Login";
+
+?>
+
+</body>
+</html>
