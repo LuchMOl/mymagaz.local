d0db00b6ad0d7223548f9ef410baca4abd04c120
LuchMOl <61159984+LuchMOl@users.noreply.github.com>
2020-02-29 14:20:35 +0200

    return-get

---
+++ app/services/routeservice.php
@@ -0,0 +1,51 @@
+﻿<?php
+
+class RouteService
+{
+	public $first;
+	public $second;
+	public $third;
+	public $route;
+	public $countSlash;
+	public $ro = array();
+
+
+		public function __construct()
+		{
+			$route = $_SERVER['REQUEST_URI'];													echo "$route<br>";
+			$countSlash = substr_count($route, '/');											echo "$countSlash<br>";
+
+			if ($countSlash === 3)
+				{
+				preg_match('/[\/](.*)\/(.*)\/(.*)/', $route, $ro);
+print_r ($ro);
+				return $first = $ro[1];
+				return $second = $ro[2];
+				return $third = $ro[3];
+				}
+				elseif ($countSlash === 2)
+					{
+					preg_match('/[\/](.*)\/(.*)/', $route, $ro);
+print_r ($ro);
+					return $first = $ro[1];
+					return $second = $ro[2];
+					}
+
+				elseif ($countSlash === 1)
+					{
+
+					preg_match('/[\/](.*)/', $route, $ro);
+print_r ($ro);
+						if (strlen($ro[1]) <> 0)
+						{
+							return $first = $ro[1];
+							echo "$first";
+						}else{
+
+							//если только домен
+						}
+					}
+		}
+}
+
+?>
--- web/index.php
+++ web/index.php
@@ -1,37 +1,27 @@
 
 <p>
-    <a href="http://mymagaz.local"><h2>mymagaz.local</h2></a>
-
-    <a href="http://mymagaz.local/tasks"><h2>tasks</h2></a>
+    <a href="/"><h2>mymagaz.local</h2></a>
+</p>
+<p>
+    <a href="/tasks"><h2>tasks</h2></a>
 </p>
 <p>
     <a href="/testhello/dima"><h2>/testhello/dima</h2></a>
 </p>
+<p>
+    <a href="/testhello/dima/33"><h2>/testhello/dima/33</h2></a>
+</p>
 
 <?php
 
-$home = "mymagaz.local";
+include '../app/services/routeservice.php';
 
-$link = $_SERVER['REQUEST_URI'];
+//$home = "mymagaz.local";
 
-$arr_str = preg_match_all("/([a-zA-z0-9]+)/", $_SERVER['REQUEST_URI'], $rez);
-//print_r ($rez[0][0]);
-//print_r ($rez[0][1]);
+$routeService = new RouteService();
 
-if ($rez[0][0] === "testhello") {
+echo "<p></p>";
 
-    echo "<h1>It is a {$rez[0][0]}.</h1>";
-    echo "Hello, {$rez[0][1]}!";
-}
+var_dump($routeService);
 
-
-if ($link === "/") {
-    echo "<h1>$home</h1>";
-}
-
-
-if ($link === "/tasks") {
-
-    require_once '../views/tasks/tasks.php';
-}
 ?>
