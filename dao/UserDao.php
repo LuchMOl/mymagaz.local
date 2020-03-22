52b6bad92a0e309664684bf513bc2d2fd45687f2
leadmitry <leadmitry@gmail.com>
2020-03-22 12:29:06 +0200

    mapper

--- dao/UserDao.php
+++ dao/UserDao.php
@@ -6,7 +6,10 @@ class UserDao
     public function getUsers()
     {
         try {
-            $dbh = new PDO('mysql:host=mymagaz.local;dbname=mymagaz', 'root');
+            $dbh = new PDO('mysql:host=mymagaz.local;dbname=mymagaz', 'root', '', [
+                \PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                , \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+            ]);
 
             foreach ($dbh->query('SELECT * from user') as $users) {
 
